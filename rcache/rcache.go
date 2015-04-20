255d0e48fd63d64a18463d554c662387ed56a00b
Maksadbek <a.maksadbek@gmail.com>
2015-04-20 16:17:31 +0500

    rcache init function tests pass

--- rcache/rcache.go
+++ rcache/rcache.go
@@ -1,9 +1,19 @@
 package rcache
 
 import (
+	"bitbucket.org/maksadbek/go-mon-service/conf"
 	"github.com/garyburd/redigo/redis"
 )
 
-func initialize() (err error) {
+var (
+	config conf.App   // config
+	rc     redis.Conn // redis client
+)
 
+func Initialize(c conf.App) (err error) {
+	rc, err = redis.Dial("tcp", c.DS.Redis.Host)
+	if err != nil {
+		return err
+	}
+	return
 }
--- rcache/rcache_test.go
+++ rcache/rcache_test.go
@@ -1,3 +1,42 @@
 package rcache
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"bitbucket.org/maksadbek/go-mon-service/conf"
+)
+
+func TestInit(t *testing.T) {
+	mockConf := `
+[ds]
+    [ds.redis]
+		host = ":6379"
+[srv]
+    port = "1234"
+[log]
+    path = "info.log"
+`
+
+	r := strings.NewReader(mockConf)
+	app, err := conf.Read(r)
+	err = Initialize(app)
+	if err != nil {
+		t.Error(err)
+	}
+	test := "bar"
+	rc.Send("SET", "foo", test)
+	rc.Send("GET", "foo")
+	rc.Flush()
+	rc.Receive()
+	v, err := rc.Receive()
+	if err != nil {
+		t.Error(err)
+	}
+	if fmt.Sprintf("%s", v) != test {
+		t.Errorf("want %s, got %s\n", v, test)
+	}
+	// close the connection
+	defer rc.Close()
+}
