9b6619c49d392feaca83dace04ef6ad249cbbb35
Maksadbek <a.maksadbek@gmail.com>
2015-04-22 16:50:13 +0500

    moved test data into testData package

--- rcache/main_test.go
+++ rcache/main_test.go
@@ -4,21 +4,11 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/garyburd/redigo/redis"
-
 	"bitbucket.org/maksadbek/go-mon-service/conf"
+	"github.com/garyburd/redigo/redis"
 )
 
 func TestMain(m *testing.M) {
-	mockConf := `
-	[ds]
-		[ds.redis]
-			host = ":6379"
-	[srv]
-		port = "1234"
-	[log]
-		path = "info.log"
-	`
 	r := strings.NewReader(mockConf)
 	app, err := conf.Read(r)
 	if err != nil {
@@ -33,6 +23,6 @@ func TestMain(m *testing.M) {
 	for _, x := range FleetTest.Trackers {
 		rc.Do("RPUSH", FleetTest.FleetName, x)
 	}
-	rc.Close()
+
 	m.Run()
 }
--- rcache/test_data.go
+++ rcache/test_data.go
@@ -7,3 +7,30 @@ var FleetTest = struct {
 	"fleet_202",
 	[]string{"id123", "id456", "id789"},
 }
+var mockConf string = `
+[ds]
+    [ds.redis]
+		host = ":6379"
+		fprefix = "fleet"
+[srv]
+    port = "1234"
+[log]
+    path = "info.log"
+`
+var testFleet Fleet = Fleet{
+	Id: "202",
+	Update: map[string]Pos{
+		"106206": Pos{
+			Id:        106206,
+			Longitude: "69.145340",
+			Latitude:  "41.260006",
+			Time:      "2015-04-21 17:59:59",
+		},
+		"107749": Pos{
+			Id:        107749,
+			Longitude: "69.245811",
+			Latitude:  "41.293964",
+			Time:      "2015-03-26 13:29:06",
+		},
+	},
+}
