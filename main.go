7270a210c027de24c6b730cdc644ab4c2c169df3
Maksadbek <a.maksadbek@gmail.com>
2015-04-24 18:35:01 +0500

    raw http package

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"bitbucket.org/maksadbek/go-mon-service/conf"
+	"bitbucket.org/maksadbek/go-mon-service/route"
+)
+
+func main() {
+	f, err := ioutil.ReadFile("conf.toml")
+	if err != nil {
+		panic(err)
+	}
+
+	c := strings.NewReader(string(f))
+	if err != nil {
+		panic(err)
+	}
+
+	app, err := conf.Read(c)
+	if err != nil {
+		panic(err)
+	}
+
+	route.Initialize(app)
+	http.HandleFunc("/", route.GetPositionHandler)
+	http.ListenAndServe(":8088", nil)
+}
--- route/route.go
+++ route/route.go
@@ -1,1 +1,28 @@
 package route
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"bitbucket.org/maksadbek/go-mon-service/conf"
+	"bitbucket.org/maksadbek/go-mon-service/rcache"
+)
+
+func Initialize(c conf.App) error {
+	err := rcache.Initialize(c)
+	if err != nil {
+		return err
+	}
+	return err
+}
+func GetPositionHandler(w http.ResponseWriter, r *http.Request) {
+	pos, err := rcache.GetPositions("fleet_202", 0, 100)
+	if err != nil {
+		panic(err)
+	}
+	jpos, err := json.Marshal(pos)
+	if err != nil {
+		panic(err)
+	}
+	w.Write([]byte(jpos))
+}
