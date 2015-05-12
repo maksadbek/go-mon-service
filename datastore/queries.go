f10ba28e9f81e9f8d99e558bef77187c8257bada
Maksadbek <a.maksadbek@gmail.com>
2015-05-12 15:44:04 +0500

    separeted sql queries into anther file

---
+++ datastore/queries.go
@@ -0,0 +1,37 @@
+package datastore
+
+var queries map[string]string = map[string]string{
+        "usrTrackers": `SELECT 
+                            login, 
+                            fleet, 
+                            cars
+                        FROM max_users
+                        WHERE login = ?
+                        LIMIT 1`,
+        "getTrackers": ` select 
+                                 id,
+                                 fleet,
+                                 imei,
+                                 number,
+                                 tracker_type,
+                                 tracker_type_id,
+                                 device_type_id,
+                                 name,
+                                 owner,
+                                 active,
+                                 additional,
+                                 customization,
+                                 group_id,
+                                 detector_fuel_id,
+                                 detector_motion_id,
+                                 detector_dinamik_id,
+                                 pid,
+                                 installed_sensor,
+                                 detector_agro_id,
+                                 car_health,
+                                 color,
+                                 what_class
+                         from
+                         max_units `,
+}
+
