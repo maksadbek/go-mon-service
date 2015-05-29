cae4d2019ae19f95b6faff672d8ff743ef7f53f6
Maksadbek <a.maksadbek@gmail.com>
2015-05-29 18:40:35 +0500

    no daemon

--- main.go
+++ main.go
@@ -1,43 +1,19 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
-	"os/exec"
-	"os/signal"
-	"strconv"
 	"strings"
-	"syscall"
 	"time"
 
 	"bitbucket.org/maksadbek/go-mon-service/conf"
 	"bitbucket.org/maksadbek/go-mon-service/datastore"
 	log "bitbucket.org/maksadbek/go-mon-service/logger"
+	"bitbucket.org/maksadbek/go-mon-service/rcache"
 	"bitbucket.org/maksadbek/go-mon-service/route"
 	"github.com/Sirupsen/logrus"
-)
-
-var (
-	control = flag.String(
-		"s",
-		"start",
-		`send signal to the daemon
-         start: start daemon
-         stop: shutdown
-         restart: reload 
-         status: check the status`)
-	confPath = flag.String(
-		"conf",
-		"conf.toml",
-		`configuration file for daemon`)
-	daemon = flag.Bool("d", true, "do not touch it")
-	sig  = make(chan os.Signal)
-	stop = make(chan bool)
-	res  = make(chan bool)
 )
 
 type Server struct {
@@ -48,122 +24,13 @@ func (srv *Server) Close() {
 	srv.Listener.Close()
 }
 
-func sendTERM(pid int) error {
-	process, err := os.FindProcess(pid)
-	if err != nil {
-		return err
-	}
-	err = process.Signal(syscall.SIGTERM)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func sendHUP(pid int) error {
-	process, err := os.FindProcess(pid)
-	if err != nil {
-		return err
-	}
-	err = process.Signal(syscall.SIGHUP)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func readPid(fileName string) (int, error) {
-	var pid int
-	p, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		return pid, err
-	}
-
-	pid, err = strconv.Atoi(string(p))
-	if err != nil {
-		return pid, err
-	}
-	return pid, nil
-}
-
-func checkPidFile(fileName string) bool {
-        _, err := os.Stat(fileName)
-        if err != nil {
-            return false
-        }
-        return true
-}
-
 var server Server
 
 func main() {
-	flag.Parse()
-	switch *control {
-	case "stop":
-		pid, err := readPid("pid")
-		if err != nil {
-			log.Log.Info("cannot read pid")
-			os.Exit(1)
-		}
-		err = sendTERM(pid)
-		if err != nil {
-			log.Log.Info("cannot send term signal to pid:", strconv.Itoa(pid))
-			os.Exit(1)
-		}
-		os.Exit(0)
-
-	case "restart":
-		pid, err := readPid("pid")
-		if err != nil {
-			log.Log.Info("cannot read pid")
-			os.Exit(1)
-		}
-		err = sendHUP(pid)
-		if err != nil {
-			log.Log.Info("cannot send term signal to pid:", strconv.Itoa(pid))
-			os.Exit(1)
-		}
-		os.Exit(0)
-    case "status":
-        if checkPidFile("pid") {
-                pid, err := readPid("pid")
-                if err != nil {
-                        panic(err)
-                }
-                fmt.Println("daemon is running, pid is: ", strconv.Itoa(pid))
-        } else {
-                fmt.Println("daemon is not running")
-        }
-        os.Exit(0)
-
-	case "start":
-        if checkPidFile("pid") {
-           fmt.Println("daemon is already running, stop it or restart")
-           os.Exit(0)
-        }
-		break
-	default:
-            fmt.Println(`  
-                        send : signal to the daemon
-                        stop : stop the daemon
-                        restart : restart the daemon
-                        status : check the status
-                        `)
-		os.Exit(1)
-	}
-	signal.Notify(sig, syscall.SIGTERM, syscall.SIGHUP)
-	go sigCatch()
-
-	go stopd()
-	go resd()
-	if *daemon == true {
-		res <- true
-		stop <- true
-	}
 	Environment := os.Getenv("GOMON")
 
 	// config init
-	f, err := ioutil.ReadFile(*confPath)
+	f, err := ioutil.ReadFile("conf.toml")
 	if err != nil {
 		panic(err)
 	}
@@ -187,95 +54,41 @@ func main() {
 
 	route.Initialize(app)
 	datastore.Initialize(app.DS)
-    go CacheData(app)
-	go worker(app)
-	err = WritePid()
-	if err != nil {
-		panic(err)
-	}
-	<-stop
-}
-
-func WritePid() error {
-	f, err := os.Create("pid")
-	if err != nil {
-		panic(err)
-	}
-	pid := os.Getpid()
-	log.Log.Info("my pid is", pid)
-	pidStr := strconv.Itoa(pid)
-	_, err = f.Write([]byte(pidStr))
-	if err != nil {
-		return err
-	}
-	f.Close()
-	return nil
-}
-func CacheData(app conf.App) {
-	datastore.CacheData()
-	for _ = range time.Tick(
-		time.Duration(app.DS.Mysql.Interval) * time.Minute) {
-		datastore.CacheData()
-	}
-}
-func sigCatch() {
-	for {
-		select {
-		case s := <-sig:
-			if s == syscall.SIGTERM {
-				log.Log.Info("got term signal")
-				stop <- true
-				break
-			} else if s == syscall.SIGHUP {
-				log.Log.Info("got hup signal")
-				server.Close()
-				res <- true
-				break
-			}
-
-		}
-	}
-}
-func stopd() {
-	<-stop
-    if checkPidFile("pid"){
-            err := os.Remove("pid")
-            if err != nil {
-                    panic(err)
-            }
-    }
-	log.Log.Info("terminating")
-	os.Exit(0)
-}
-
-func resd() {
-	<-res
-	log.Log.Info("restarting")
-	logFile, err := os.Create("logs")
-	if err != nil {
-		panic(err)
-	}
-	defer logFile.Close()
-	cmd := exec.Command(os.Args[0], "-d=false")
-	cmd.Stdout = logFile
-	cmd.Stderr = logFile
-	err = cmd.Start()
-	if err != nil {
-		panic(err)
-	}
-	stop <- true
-}
-
-func worker(app conf.App) {
-	var err error
-	server.Listener, err = net.Listen("tcp", ":1234")
+	rcache.Initialize(app)
+	go CacheData(app)
+	server.Listener, err = net.Listen("tcp", app.SRV.Port)
 	if err != nil {
 		panic(err)
 	}
 	serve := &http.Server{Handler: webHandlers()}
+	log.Log.Info("Serving HTTP server on " + app.SRV.Port + " port")
 	serve.Serve(server.Listener)
 }
 
+func CacheData(app conf.App) {
+	trackers, err := datastore.GetTrackers("")
+	if err != nil {
+		panic(err)
+	}
+	rcache.CacheDefaults(trackers)
+	CacheFleetTrackers()
+	for _ = range time.Tick(time.Duration(app.DS.Mysql.Interval) * time.Minute) {
+		trackers, err := datastore.GetTrackers("")
+		if err != nil {
+			panic(err)
+		}
+		rcache.CacheDefaults(trackers)
+		CacheFleetTrackers()
+	}
+}
+
+func CacheFleetTrackers() {
+	t, err := datastore.CacheFleetTrackers()
+	if err != nil {
+		panic(err)
+	}
+	rcache.AddFleetTrackers(t)
+}
 func webHandlers() http.Handler {
 	web := http.NewServeMux()
 	web.Handle("/", http.FileServer(http.Dir("static/")))
