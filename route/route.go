7a7b7088c4178b93d3770419aac4dad6e5387698
Maksadbek <a.maksadbek@gmail.com>
2015-06-01 18:28:59 +0500

    used logger wrapper functions

--- route/route.go
+++ route/route.go
@@ -2,15 +2,12 @@ package route
 
 import (
 	"encoding/json"
-	"fmt"
-	"math/rand"
 	"net/http"
 
 	"bitbucket.org/maksadbek/go-mon-service/conf"
 	"bitbucket.org/maksadbek/go-mon-service/datastore"
-	log "bitbucket.org/maksadbek/go-mon-service/logger"
+	"bitbucket.org/maksadbek/go-mon-service/logger"
 	"bitbucket.org/maksadbek/go-mon-service/rcache"
-	"github.com/Sirupsen/logrus"
 )
 
 var config conf.App
@@ -27,30 +24,14 @@ func GetPositionHandler(w http.ResponseWriter, r *http.Request) {
 	fleetName, user, groups := r.PostFormValue("fleet"), r.PostFormValue("user"), r.PostFormValue("groups")
 
 	if fleetName == "" || user == "" || groups == "" {
-		log.Log.WithFields(logrus.Fields{
-			"GET Request": "/positions",
-			"fleet":       fleetName,
-			"user":        user,
-			"groups":      groups,
-			"http status": 404,
-		}).Warn("Request Error")
+		logger.ReqWarn(r, conf.ErrReq)
 		http.Error(w, config.ErrorMsg["NotExistInCache"].Msg, 404)
 		return
 	}
-	log.Log.WithFields(logrus.Fields{
-		"GET Request": "/positions",
-		"fleet":       fleetName,
-		"user":        user,
-		"groups":      groups,
-	}).Info("Request")
-
+	logger.ReqWarn(r, conf.ErrReq)
 	trackers, err := GetTrackers(user)
 	if err != nil {
-		log.Log.WithFields(logrus.Fields{
-			"GET Request": "/positions",
-			"error":       err.Error(),
-			"http status": 404,
-		}).Warn("Request Error")
+		logger.ReqWarn(r, conf.ErrReq, err)
 		http.Error(w, err.Error(), 404)
 		return
 	}
@@ -59,28 +40,14 @@ func GetPositionHandler(w http.ResponseWriter, r *http.Request) {
 	if trackers.Trackers[0] == "0" {
 		fleet, err = rcache.GetPositionsByFleet(fleetName, 0, 100)
 		if err != nil {
-			log.Log.WithFields(logrus.Fields{
-				"GET Request": "/positions",
-				"fleet":       fleetName,
-				"user":        user,
-				"groups":      groups,
-				"error":       err.Error(),
-				"http status": 404,
-			}).Warn("Request Error")
+			logger.ReqWarn(r, conf.ErrReq, err)
 			http.Error(w, err.Error(), 404)
 			return
 		}
 	} else {
 		pos, err := rcache.GetPositions(trackers.Trackers)
 		if err != nil {
-			log.Log.WithFields(logrus.Fields{
-				"GET Request": "/positions",
-				"fleet":       fleetName,
-				"user":        user,
-				"groups":      groups,
-				"error":       err.Error(),
-				"http status": 404,
-			}).Warn("Request Error")
+			logger.ReqWarn(r, conf.ErrReq, err)
 			http.Error(w, err.Error(), 404)
 			return
 		}
@@ -90,47 +57,26 @@ func GetPositionHandler(w http.ResponseWriter, r *http.Request) {
 
 	jpos, err := json.Marshal(fleet)
 	if err != nil {
-		log.Log.WithFields(logrus.Fields{
-			"GET Request": "/positions",
-			"fleet":       fleetName,
-			"user":        user,
-			"groups":      groups,
-			"error":       err.Error(),
-			"http status": 404,
-		}).Warn("Request Error")
+		logger.ReqWarn(r, conf.ErrReq, err)
 		http.Error(w, err.Error(), 404)
 		return
 	}
-
 	w.Write([]byte(jpos))
-}
-func randInt(min int, max int) int {
-	return min + rand.Intn(max-min)
+	return
 }
 
 func GetTrackers(name string) (rcache.Usr, error) {
 	trackers, err := rcache.UsrTrackers(name)
-	log.Log.WithFields(logrus.Fields{
-		"user":    fmt.Sprintf("%v", trackers),
-		"package": "route",
-	}).Info("GetTrackers")
+	logger.FuncLog("route.GetTracker", "GetTracker", nil, nil)
 	if err != nil {
 		if err.Error() == config.ErrorMsg["NotExistInCache"].Msg {
 			trackersDS, err := datastore.UsrTrackers(name)
 			if err != nil {
-				log.Log.WithFields(logrus.Fields{
-					"error":   err.Error(),
-					"package": "route",
-				}).Warn("Request Error")
-				//TODO http.Error(w, err.Error(), 404)
 				return trackers, err
 			}
 			err = rcache.SetUsrTrackers(trackersDS)
 			if err != nil {
-				log.Log.WithFields(logrus.Fields{
-					"error":   err.Error(),
-					"package": "route",
-				}).Warn("GetTrackers")
+				logger.FuncLog("route.GetTrackers", "GetTrackers", nil, err)
 				return trackersDS, err
 			}
 			return trackersDS, nil
