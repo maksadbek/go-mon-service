a98b99e07241144a77d91aa9931b801abe470e41
maksadbek <max@localhost.localdomain>
2015-07-11 17:20:09 +0500

    changing the style of parent div, to enable scrolling

--- static/flux/js/app.js
+++ static/flux/js/app.js
@@ -1,7 +1,7 @@
 var React = require('react');
 var StatusApp = require('./components/StatusApp.react');
 
-React.render(
-        <StatusApp/>,
-        document.getElementById('ext-gen91')
-);
+React.render( <StatusApp/>, document.getElementById('ext-gen91'));
+var e = document.getElementById('ext-gen91');
+e.style.cssText = "overflow: scroll";
+
