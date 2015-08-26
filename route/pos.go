5af9486dc3ef102bbd07c35e47e1e4e0bd3d5f53
Maksadbek <a.maksadbek@gmail.com>
2015-08-26 13:59:20 +0500

    added rate limiter and JSON type responses

--- route/pos.go
+++ route/pos.go
@@ -11,48 +11,76 @@ import (
 	"bitbucket.org/maksadbek/go-mon-service/conf"
 	"bitbucket.org/maksadbek/go-mon-service/logger"
 	"bitbucket.org/maksadbek/go-mon-service/rcache"
+	"github.com/didip/tollbooth"
 )
 
 func GetPositionHandler(w http.ResponseWriter, r *http.Request) {
+	response := struct {
+		Message string `json:"message"`
+	}{}
+	tok, ok := r.Header["X-Access-Token"]
+	if !ok || len(tok) == 0 {
+		response.Message = "Missing Token Key"
+		message, _ := json.Marshal(response)
+		http.Error(w, string(message), 400)
+		return
+	}
+	token := tok[0]
+
+	httpError := tollbooth.LimitByKeys(Limiter, tok)
+	if httpError != nil {
+		response.Message = httpError.Error()
+		message, _ := json.Marshal(response)
+		http.Error(w, string(message), 400)
+		return
+	}
 	// decode request values
 	decoder := json.NewDecoder(r.Body)
 	req := make(map[string]string)
 	err := decoder.Decode(&req)
 	if err != nil {
 		logger.ReqWarn(r, conf.ErrReq)
-		http.Error(w, "invalid req body format", 500)
+		response.Message = "invalid req body format"
+		message, _ := json.Marshal(response)
+		http.Error(w, string(message), 400)
 		return
 	}
 
 	// retrieve data
-	fleetName := req["selectedFleetJs"]
-	user := req["user"]
-	groups := req["groups"]
-	token := req["token"]
+	fleetName := req["fleetID"]
+	user := req["userName"]
 
 	// validate for empty
-	if fleetName == "" || user == "" || groups == "" || token == "" {
+	if fleetName == "" || user == "" {
 		logger.ReqWarn(r, conf.ErrReq)
-		http.Error(w, conf.ErrInvalidReq, 400)
+		response.Message = "Missing Fields"
+		message, _ := json.Marshal(response)
+		http.Error(w, string(message), 400)
 		return
 	}
 
 	// check token
-	usrTokenKey, ok := tokenList.Get(token)
+	usrTokenKey, ok := TokenList.Get(token)
 	if !ok {
-		http.Error(w, conf.ErrUnauthReq, 511)
+		response.Message = conf.ErrUnauthReq
+		message, _ := json.Marshal(response)
+		http.Error(w, string(message), 511)
 		return
 	}
 	expectedToken := computeHMAC(user, usrTokenKey.Key)
 	if token != base64.StdEncoding.EncodeToString(expectedToken) {
-		http.Error(w, conf.ErrUnauthReq, 511)
+		response.Message = conf.ErrUnauthReq
+		message, _ := json.Marshal(response)
+		http.Error(w, string(message), 511)
 		return
 	}
 	logger.ReqWarn(r, conf.ErrReq)
 	trackers, err := GetTrackers(user)
 	if err != nil {
 		logger.ReqWarn(r, conf.ErrReq, err)
-		http.Error(w, err.Error(), 404)
+		response.Message = err.Error()
+		message, _ := json.Marshal(response)
+		http.Error(w, string(message), 404)
 		return
 	}
 	var fleet rcache.Fleet
@@ -61,25 +89,30 @@ func GetPositionHandler(w http.ResponseWriter, r *http.Request) {
 		fleet, err = rcache.GetPositionsByFleet(fleetName, 0, 100)
 		if err != nil {
 			logger.ReqWarn(r, conf.ErrReq, err)
-			http.Error(w, err.Error(), 404)
+			response.Message = err.Error()
+			message, _ := json.Marshal(response)
+			http.Error(w, string(message), 404)
 			return
 		}
 	} else {
 		pos, err := rcache.GetPositions(trackers.Trackers)
 		if err != nil {
 			logger.ReqWarn(r, conf.ErrReq, err)
-			http.Error(w, err.Error(), 404)
+			response.Message = err.Error()
+			message, _ := json.Marshal(response)
+			http.Error(w, string(message), 404)
 			return
 		}
 		fleet.Update = pos
 		fleet.Id = fleetName
 	}
-
 	fleet.LastReq = time.Now().Unix()
 	jpos, err := json.Marshal(fleet)
 	if err != nil {
 		logger.ReqWarn(r, conf.ErrReq, err)
-		http.Error(w, err.Error(), 404)
+		response.Message = err.Error()
+		message, _ := json.Marshal(response)
+		http.Error(w, string(message), 404)
 		return
 	}
 	w.Write([]byte(jpos))
